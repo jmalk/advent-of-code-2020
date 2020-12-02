eb421520dc901e4acfa7346e56f3a8f3b588403e
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-12-02 19:30:06 +0000

    Format

--- day-02/part-02.py
+++ day-02/part-02.py
@@ -1,36 +1,38 @@
 with open('input.txt', 'r') as f:
-  lines = f.readlines()
+    lines = f.readlines()
 f.close()
 
 lines = [l.replace("\n", "") for l in lines]
 
+
 def isValid(password, letter, firstPosition, secondPosition):
-  firstIndex = firstPosition - 1
-  secondIndex = secondPosition - 1
-  occurrences = 0
+    firstIndex = firstPosition - 1
+    secondIndex = secondPosition - 1
+    occurrences = 0
 
-  if password[firstIndex] == letter:
-    occurrences = occurrences + 1
+    if password[firstIndex] == letter:
+        occurrences = occurrences + 1
 
-  if password[secondIndex] == letter:
-    occurrences = occurrences + 1
+    if password[secondIndex] == letter:
+        occurrences = occurrences + 1
 
-  return occurrences == 1
+    return occurrences == 1
+
 
 validPasswords = 0
 
 for line in lines:
-  lineParts = line.split()
-  firstPositionStr = lineParts[0].split("-")[0]
-  firstPosition = int(firstPositionStr)
+    lineParts = line.split()
+    firstPositionStr = lineParts[0].split("-")[0]
+    firstPosition = int(firstPositionStr)
 
-  secondPositionStr = lineParts[0].split("-")[1]
-  secondPosition = int(secondPositionStr)
+    secondPositionStr = lineParts[0].split("-")[1]
+    secondPosition = int(secondPositionStr)
 
-  requiredLetter = lineParts[1].split(":")[0]
-  password = lineParts[2]
+    requiredLetter = lineParts[1].split(":")[0]
+    password = lineParts[2]
 
-  if isValid(password, requiredLetter, firstPosition, secondPosition):
-    validPasswords = validPasswords + 1
+    if isValid(password, requiredLetter, firstPosition, secondPosition):
+        validPasswords = validPasswords + 1
 
 print(validPasswords)
