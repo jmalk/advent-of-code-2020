0a9290bed0af0593b96dc69cb1aded6daf595531
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-12-04 11:05:09 +0000

    Refactor

--- day-04/validators/birthyear.py
+++ day-04/validators/birthyear.py
@@ -1,6 +1,9 @@
 import re
 
 
+def is_four_digits(string):
+    return re.match(r'\d{4}$', string) != None
+
+
 def is_valid_birth_year(birth_year):
-    return re.match(r'\d{4}$', birth_year) != None and int(
-        birth_year) >= 1920 and int(birth_year) <= 2020
+    return is_four_digits(birth_year) and 1920 <= int(birth_year) <= 2020
