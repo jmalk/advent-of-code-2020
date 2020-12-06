747f6f63c31ae6907d1b6b84f0dd9704e28f0918
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-12-06 16:48:51 +0000

    Make variable names a bit clearer

--- day-06/part-01.py
+++ day-06/part-01.py
@@ -2,7 +2,7 @@ with open('input.txt', 'r') as f:
     file_string = ''.join(f.readlines())
 f.close()
 
-group_strings = [s.replace('\n', '') for s in file_string.split('\n\n')]
-sum_uniques = sum([len(set(g)) for g in group_strings])
+group_answers = [s.replace('\n', '') for s in file_string.split('\n\n')]
+sum_uniques = sum([len(set(answers)) for answers in group_answers])
 assert sum_uniques == 6457
 print('Part 1: ', sum_uniques)
