62613405dc72dd71ad3bf9c53b452b63ef2cdc23
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2020-12-08 17:36:16 +0000

    Solve day 8 part 2

--- day-08/parts-01-and-02.py
+++ day-08/parts-01-and-02.py
@@ -28,74 +28,111 @@ def process_instruction(instruction, program_state):
     pointer = program_state["pointer"]
 
     if instruction_type == 'acc':
-        return {"acc": acc + increment, "pointer": pointer + 1}
+        return {"acc": acc + increment, "pointer": pointer + 1, "error": None}
     if instruction_type == 'jmp':
-        return {"acc": acc, "pointer": pointer + increment}
+        return {"acc": acc, "pointer": pointer + increment, "error": None}
     # Default to 'nop' case
-    return {"acc": acc, "pointer": pointer + 1}
+    return {"acc": acc, "pointer": pointer + 1, "error": None}
 
 
 # nop should move on to next instruction (increment pointer) and do nothing else
 assert process_instruction(('nop', +0), {
     "acc": 0,
-    "pointer": 0
+    "pointer": 0,
+    "error": None
 }) == {
     "acc": 0,
-    "pointer": 1
+    "pointer": 1,
+    "error": None
 }
 
 assert process_instruction(('nop', +0), {
     "acc": 11,
-    "pointer": 3
+    "pointer": 3,
+    "error": None
 }) == {
     "acc": 11,
-    "pointer": 4
+    "pointer": 4,
+    "error": None
 }
 
 # acc should add to accumulator value and move on to next instruction (increment pointer)
 assert process_instruction(('acc', +7), {
     "acc": 0,
-    "pointer": 0
+    "pointer": 0,
+    "error": None
 }) == {
     "acc": 7,
-    "pointer": 1
+    "pointer": 1,
+    "error": None
 }
 
 assert process_instruction(('acc', +7), {
     "acc": -3,
-    "pointer": 12
+    "pointer": 12,
+    "error": None
 }) == {
     "acc": 4,
-    "pointer": 13
+    "pointer": 13,
+    "error": None
 }
 
 # jmp should move pointer by value without changing accumulator
 assert process_instruction(('jmp', +2), {
     "acc": 0,
-    "pointer": 6
+    "pointer": 6,
+    "error": None
 }) == {
     "acc": 0,
-    "pointer": 8
+    "pointer": 8,
+    "error": None
 }
 
+error_codes = {'INFINITE': 'infinite loop'}
 
-def accumulator_before_repeat(program):
+
+def run_program(program):
     lines_run = []
-    would_repeat = False
-    state = {'acc': 0, 'pointer': 0}
+    ended = False
+    state = {'acc': 0, 'pointer': 0, 'error': None}
 
-    while would_repeat == False:
-        if state['pointer'] not in lines_run:
+    while ended == False:
+        # If program reaches the index immediately after instructions,
+        # it terminates
+        if state['pointer'] == len(program):
+            ended = True
+        elif state['pointer'] not in lines_run:
             lines_run.append(state['pointer'])
             state = process_instruction(program[state['pointer']], state)
         else:
-            would_repeat = True
+            ended = True
+            state['error'] = error_codes['INFINITE']
 
-    return state['acc']
+    return state
 
 
-assert accumulator_before_repeat(sample_instructions) == 5
+assert run_program(sample_instructions)['acc'] == 5
+assert run_program(sample_instructions)['error'] == error_codes['INFINITE']
 
-part_1 = accumulator_before_repeat(instructions)
+part_1 = run_program(instructions)['acc']
 print('Part 1:', part_1)
 assert part_1 == 1451
+
+
+def try_fixing_program(program):
+    swaps = {'jmp': 'nop', 'nop': 'jmp'}
+
+    for i in range(len(program)):
+        instruction = program[i]
+        instruction_type = instruction[0]
+        if instruction_type in swaps:
+            program_copy = program.copy()
+            program_copy[i] = (swaps[instruction_type], program_copy[i][1])
+            result = run_program(program_copy)
+            if result['error'] == None:
+                return result['acc']
+
+
+assert try_fixing_program(sample_instructions) == 8
+part_2 = try_fixing_program(instructions)
+print('Part 2:', part_2)
